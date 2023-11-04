746dc00544eae53c82ef4d87875c481469e45455
Syed Musa <musabinsyed@gmail.com>
2023-11-05 01:02:02 +0600

    added a navbar and footer section

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
 const Navbar = () => {
+
+    const navItems = <>
+    <li className="font-bold uppercase text-white text-[15px]"><Link>Home</Link></li>
+    <li className="font-bold uppercase text-white text-[15px]"><Link>Add Job</Link></li>
+    <li className="font-bold uppercase text-white text-[15px]"><Link>My Posted Jobs</Link></li>
+    <li className="font-bold uppercase text-white text-[15px]"><Link>My Bids</Link></li>
+    <li className="font-bold uppercase text-white text-[15px]"><Link>Bids Request</Link></li>
+    </>
+
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="my-10">
+      <div className="navbar bg-gradient-to-r from-blue-400 to-indigo-400">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -21,29 +33,24 @@ const Navbar = () => {
               </svg>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              {navItems}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <div className="flex items-center text-xl">
+          <img className="w-28 h-14" src={logo} alt="" />
+          <p className="font-bold uppercase italic text-white">OnlineMarkets</p>
+          </div>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li><a>Item 3</a></li>
+            {navItems}
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn bg-indigo-600 uppercase text-white">Login</a>
         </div>
       </div>
+      
     </div>
   );
 };
