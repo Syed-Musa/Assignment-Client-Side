8edaca21e23ad27e45588e892825dd80627d379d
Syed Musa <musabinsyed@gmail.com>
2023-11-05 11:58:06 +0600

    added a firebase section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,9 @@
+// import ReactTabs from "../Tabs/ReactTabs";
 
 const HomePage = () => {
     return (
         <div>
+            {/* <ReactTabs></ReactTabs> */}
             <h3 className="text-6xl">This is homepage</h3>
         </div>
     );
---
+++ src/Tabs/ReactTabs.jsx
@@ -0,0 +1,34 @@
+// import { useState } from "react";
+
+// const ReactTabs = () => {
+
+    
+//   return (
+//     <div className=" border-2">
+//       <div className="tabs tabs-boxed justify-around">
+//         <a className="tab tab-active block">Tab 1</a>
+//         <a className="tab">Tab 2</a>
+//         <a className="tab">Tab 3</a>
+//       </div>
+
+//       <div className="relative ">
+//         <div className="hidden">
+//         <div className="content1">
+//             <h2>content1</h2>
+//             <p>This is content 1</p>
+//         </div>
+//         <div className="content2">
+//             <h2>content2</h2>
+//             <p>This is content 2</p>
+//         </div>
+//         <div className="content3">
+//             <h2>content3</h2>
+//             <p>This is content 3</p>
+//         </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ReactTabs;
